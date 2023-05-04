ce88ce2466c81be3f7ec8b66e95551a743b200e4
Alex Brown <alex@brownleeschools.com>
2023-05-04 17:59:25 +0100

    add navigation to titlebar

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,76 +3,74 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Snackbar from '@mui/material/Snackbar';
-import StudentSearch from './StudentSearch';
-import WhoIsDoing from './WhoIsDoing';
 
 import { SERVER_URL } from '../constants.js';
 
-function Login() {
-  const [isAuthenticated, setAuth] = useState(false);
-  const [user, setUser] = useState({
-        email: '', 
-        password: ''
-      });
-    const [open, setOpen] = useState(false);
+function Login(props) {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [isError, setIsError] = useState(false);
 
-    const handleChange = (event) => {
-        setUser({...user, [event.target.name] : event.target.value});
-      }
+  const [user, setUser] = useState({
+    email: '', 
+    password: ''
+  });
+  
+  const handleChange = (event) => {
+    setUser({...user, [event.target.name] : event.target.value});
+  }
       
-      const login = () => {
-        fetch(SERVER_URL + 'api/v1/auth/authenticate', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(user)
-        })
-        .then(response => response.json())
-        .then(data => {
-          if (data.access_token !== undefined) {
-            //setAccessToken(data.access_token);
-            sessionStorage.setItem('bearer', data.access_token);
-            sessionStorage.setItem('isAuthenticated', true);
-            setAuth(true);
-          } else {
-            setOpen(true);
+  const login = () => {
+    fetch(SERVER_URL + 'api/v1/auth/authenticate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.access_token !== undefined) {
+        //setAccessToken(data.access_token);
+        sessionStorage.setItem('bearer', data.access_token);
+        sessionStorage.setItem('isAuthenticated', true);
+        setMessage('Login successful!');
+        setIsError(false);
+        setOpen(true);
+        props.onLoginSuccess();
+      } else {
+        setMessage('Login failed: Check your username and password');
+        setIsError(true);
+        setOpen(true);
           }
-        })
-        .catch(err => console.error(err));
-      };        
-      if (isAuthenticated) {
-        //return <StudentSearch  />;
-        return <WhoIsDoing />;
-      }
-      else {  
-        return(
-        <div>
-          <Stack spacing={2} alignItems='center' mt={2}>
-          <TextField 
-            name="email"
-            label="Email" 
-            onChange={handleChange} />
-          <TextField 
-            type="password"
-            name="password"
-            label="Password"
-            onChange={handleChange}/>
-          <Button 
-            variant="outlined" 
-            color="primary" 
-            onClick={login}>
-              Login
-          </Button>
-        </Stack>
-        <Snackbar 
-          open={open}
-          autoHideDuration={3000}
-          onClose={() => setOpen(false)}
-          message="Login failed: Check your username and password"
+    })
+    .catch(err => console.error(err));
+  };        
+  
+  return (
+    <div>
+      <Stack spacing={2} alignItems="center" mt={2}>
+        <TextField name="email" label="Email" onChange={handleChange} />
+        <TextField
+          type="password"
+          name="password"
+          label="Password"
+          onChange={handleChange}
         />
-        </div>
-      
-    );
-        }
-}
+        <Button variant="outlined" color="primary" onClick={login}>
+          Login
+        </Button>
+      </Stack>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={() => setOpen(false)}
+        message={message}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        ContentProps={{
+          style: { backgroundColor: isError ? '#f44336' : '#4caf50' },
+        }}
+      />
+    </div>
+  );
+  }
 
 export default Login;
