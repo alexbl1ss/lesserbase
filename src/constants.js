5ea8c42a9045aaf6b3363bb833e028e146078186
alexbl1ss <127896474+alexbl1ss@users.noreply.github.com>
2023-05-14 17:41:54 +0100

    point back at main test service

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const SERVER_URL='https://blissdb.herokuapp.com/';
-//export const SERVER_URL='https://blissdbtest.herokuapp.com/';
+//export const SERVER_URL='https://blissdb.herokuapp.com/';
+export const SERVER_URL='https://blissdbtest.herokuapp.com/';
 //export const SERVER_URL='http://localhost:8080/'
 //export const SERVER_URL='https://blissalex.herokuapp.com/';
