8c4ea3b3043a4896edb08b54dabc423fef997618
alexbl1ss <127896474+alexbl1ss@users.noreply.github.com>
2024-05-29 21:16:48 +0100

    initial deployment steps

---
+++ Deploy/productFixes.sql
@@ -0,0 +1,12 @@
+select * from booking where product_id in (971,975,979,983,987,1015,1012,959,963,967);
+
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '971');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '975');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '979');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '983');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '987');
+UPDATE `product` SET `end_date` = '2024-07-06', `start_date` = '2024-07-06' WHERE (`id` = '1015');
+UPDATE `product` SET `end_date` = '2024-07-07', `start_date` = '2024-07-07' WHERE (`id` = '1012');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '959');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '963');
+UPDATE `product` SET `end_date` = '2024-07-05', `start_date` = '2024-07-01' WHERE (`id` = '967');
