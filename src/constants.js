82e70c03f4cb73127cb336d5675b71b9694e2739
alexbl1ss <127896474+alexbl1ss@users.noreply.github.com>
2024-06-11 22:19:33 +0100

    Update constants.js point back

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const SERVER_URL='https://blissdb.herokuapp.com/';
-//export const SERVER_URL='https://blissdbtest.herokuapp.com/';
+//export const SERVER_URL='https://blissdb.herokuapp.com/';
+export const SERVER_URL='https://blissdbtest.herokuapp.com/';
 //export const SERVER_URL='http://localhost:8080/'
 //export const SERVER_URL='https://blissalex.herokuapp.com/';
 
