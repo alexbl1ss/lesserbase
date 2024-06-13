9a96b39f5de56c0d40da2c1d1d038bda98ee0fbd
alexbl1ss <127896474+alexbl1ss@users.noreply.github.com>
2024-06-13 17:02:22 +0100

    minimised back end calls for date scheduling

--- src/components/AllTabs/Scheduler.js
+++ src/components/AllTabs/Scheduler.js
@@ -14,9 +14,6 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { Box, ThemeProvider, createTheme } from '@mui/material';
 import InputIcon from "react-multi-date-picker/components/input_icon"
 
-
-
-
 function Scheduler(props) {
   const [campDates, setCampDates] = useState([]);
   const [allGroups, setAllGroups] = useState([]);
@@ -27,139 +24,160 @@ function Scheduler(props) {
   const [selectedGroupType, setSelectedGroupType] = useState('');
   const [selectAll, setSelectAll] = useState(false);
   const [selectedConvertedDates, setSelectedConvertedDates] = useState([]);
+  const [previousSelectedDates, setPreviousSelectedDates] = useState([]);
 
-
-    const fetchDates = useCallback(() => {
+  const fetchDates = useCallback(() => {
+    console.log("when does fetchDates get called?")
     const token = sessionStorage.getItem('bearer');
+  
     fetch(`${SERVER_URL}api/camptimes`, {
       headers: { Authorization: `Bearer ${token}` },
     })
-      .then((response) => {
-        if (response.status === 204) {
-          return [];
-        } else if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Failed to fetch camptimes');
-        }
-      })
-      .then((data) => {
-        sessionStorage.setItem('camptimes', JSON.stringify(data));
-        setCampDates(data);
-      })
-      .catch((err) => console.error(err));
+    .then((response) => {
+      if (response.status === 204) {
+        return [];
+      } else if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch camptimes');
+      }
+    })
+    .then((data) => {
+      sessionStorage.setItem('camptimes', JSON.stringify(data));
+      setCampDates(data);
+    })
+    .catch((err) => console.error(err));
   }, []);
 
   const convertedCampDates = campDates.map(date => {
+    console.log("when does convertedCampDates get called?")
     return {
       ...date,
-      campDate: new Date(date.campDate) // Convert string to Date object
+      campDate: new Date(date.campDate)
     };
   });
-  
 
-  const fetchGroups = useCallback(() => {
-    const token = sessionStorage.getItem('bearer');
-    fetch(`${SERVER_URL}api/campgroups`, {
-        headers: { Authorization: `Bearer ${token}` },
-    })
-        .then(response => response.json())
-        .then(data => {
-            sessionStorage.setItem('campgroups', JSON.stringify(data));
-            setAllGroups(data);
-            setFilteredGroups(data);
-        })
-        .catch(err => console.error(err));
-}, []);
-
-useEffect(() => {
-  fetchDates();
-  fetchGroups();
-}, [fetchDates, fetchGroups]);
-
-useEffect(() => {
-  // Automatically select the first group if there's only one after filtering
-  if (filteredGroups.length === 1) {
-    const groupToSelect = filteredGroups[0];
-    setSelectedGroup(groupToSelect);
-    handleGroupChangeAfterSelection(groupToSelect);  // handle side effects
-  } else if (filteredGroups.length === 0) {
-    setSelectedGroup('');
-  }
-}, [filteredGroups]);
-
-useEffect(() => {
-  // Check if all current visible dates are selected
-  if (campDates.length > 0 && selectedDates.length === campDates.length) {
-    setSelectAll(true);
-  } else {
-    setSelectAll(false);
-  }
-}, [selectedDates, campDates]);
-
-
-const handleSelectAll = (event) => {
-  if (event.target.checked) {
-    // Add all displayed dates to selectedDates
-    setSelectedDates(campDates);
-  } else {
-    // Remove all displayed dates from selectedDates
-    setSelectedDates(selectedDates.filter(date => !campDates.some(campDate => campDate.id === date.id)));
-  }
-  setSelectAll(event.target.checked);
-};
-
-
-
-// Function to handle additional logic when a group is automatically selected
-const handleGroupChangeAfterSelection = (group) => {
-  // Assuming you need to filter or set dates or other related state changes
-  const datesWithGroup = campDates.filter(date => 
-    date.groups.some(g => g.id === group.id)
-  );
-  setSelectedDates(datesWithGroup);
-
-  const ConvertedDatesWithGroup = convertedCampDates.filter(date => 
-    date.groups.some(g => g.id === group.id)
-  );
-  setSelectedConvertedDates(ConvertedDatesWithGroup.map(date => date.campDate));
-};
-
-const handleGroupTypeChange = (event) => {
-  const groupType = event.target.value;
-  setSelectedGroupType(groupType);
-  filterGroups(selectedCampus, groupType);
-};
-
-const handleCampusChange = (event) => {
-  const campus = event.target.value;
-  setSelectedCampus(campus);
-  filterGroups(campus, selectedGroupType);
-};
-
-const filterGroups = (campus, groupType) => {
-  const filtered = allGroups.filter(group => {
-      return (!campus || group.campus === campus) && (!groupType || group.groupType === groupType);
-  });
-  setFilteredGroups(filtered);
-
-  if (filtered.length > 0) {
-    const firstGroup = filtered[0];
-    setSelectedGroup(firstGroup); // Ensure this updates the Select component
-    handleGroupChangeAfterSelection(firstGroup);
-  } else {
-    setSelectedGroup(''); // Reset if no groups are available after filter
-    setSelectedDates([]); // Clear selected dates when no groups match the filter
-  }
-};
-
-
-const sortedDates = campDates.sort((a, b) => {
+  const sortedDates = campDates.sort((a, b) => {
+    console.log("is sortedDates called?")
     return new Date(a.campDate) - new Date(b.campDate);
   });
+
+  const fetchGroups = useCallback(() => {
+    console.log("when does fetchGroups get called?")
+    const token = sessionStorage.getItem('bearer');
+    fetch(`${SERVER_URL}api/campgroups`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then(response => response.json())
+      .then(data => {
+        sessionStorage.setItem('campgroups', JSON.stringify(data));
+        setAllGroups(data);
+        setFilteredGroups(data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    console.log("fetcher effect");
+    fetchDates();
+    fetchGroups();
+  }, [fetchDates, fetchGroups]);
+
+  useEffect(() => {
+    console.log("group change effect");
+    if (filteredGroups.length === 1) {
+      const groupToSelect = filteredGroups[0];
+      setSelectedGroup(groupToSelect);
+      handleGroupChangeAfterSelection(groupToSelect);  // handle side effects
+    } else if (filteredGroups.length === 0) {
+      setSelectedGroup('');
+    }
+  }, [filteredGroups]);
+
+  useEffect(() => {
+    console.log("dates effect");
+    if (campDates.length > 0 && selectedDates.length === campDates.length) {
+      setSelectAll(true);
+    } else {
+      setSelectAll(false);
+    }
+  }, [selectedDates, campDates]);
+
+  const handleSelectAll = (event) => {
+    console.log("select all dates")
+    if (event.target.checked) {
+      setSelectedDates(campDates);
+    } else {
+      setSelectedDates(selectedDates.filter(date => !campDates.some(campDate => campDate.id === date.id)));
+    }
+    setSelectAll(event.target.checked);
+  };
+
+  const handleGroupTypeChange = (event) => {
+    console.log("handleGroupTypeChange")
+    const groupType = event.target.value;
+    setSelectedGroupType(groupType);
+    filterGroups(selectedCampus, groupType);
+  };
   
+  const handleCampusChange = (event) => {
+    console.log("handleCampusChange")
+    const campus = event.target.value;
+    setSelectedCampus(campus);
+    filterGroups(campus, selectedGroupType);
+  };
   
-  const handleDatesSelect = useCallback((campdate) => {
+  const filterGroups = (campus, groupType) => {
+    console.log("filterGroups")
+    const filtered = allGroups.filter(group => {
+        return (!campus || group.campus === campus) && (!groupType || group.groupType === groupType);
+    });
+    setFilteredGroups(filtered);
+  
+    if (filtered.length > 0) {
+      const firstGroup = filtered[0];
+      setSelectedGroup(firstGroup);
+      handleGroupChangeAfterSelection(firstGroup);
+    } else {
+      setSelectedGroup('');
+      setSelectedDates([]);
+    }
+  };
+  
+  const handleGroupChangeAfterSelection = (group) => {
+    console.log("handleGroupChangeAfterSelection")  
+
+    if (!selectedGroup || group.id !== selectedGroup.id) {
+      const datesWithGroup = campDates.filter(date => 
+        date.groups.some(g => g.id === group.id)
+      );
+      setSelectedDates(datesWithGroup);
+      setPreviousSelectedDates(datesWithGroup);
+
+      console.log("selected dates");
+      console.log(selectedDates);
+      console.log("Pre selected dates");
+      console.log(previousSelectedDates);
+
+      const ConvertedDatesWithGroup = convertedCampDates.filter(date => 
+        date.groups.some(g => g.id === group.id)
+      );
+
+      setSelectedConvertedDates(ConvertedDatesWithGroup.map(date => date.campDate));
+    }
+    console.log("selected dates (converted)");
+    console.log(selectedConvertedDates)
+};
+
+const handleGroupChange = (event) => {
+  console.log("handleGroupChangeAfterSelection")
+  const group = event.target.value;
+  setSelectedGroup(group);
+  handleGroupChangeAfterSelection(group);
+};
+
+const handleDatesSelect = useCallback((campdate) => {
+    console.log("When is handleDatesSelect called?")
     const isSelected = selectedDates.some(date => date.id === campdate.id);
     if (isSelected) {
         setSelectedDates(selectedDates.filter(date => date.id !== campdate.id));
@@ -168,49 +186,46 @@ const sortedDates = campDates.sort((a, b) => {
     }
 }, [selectedDates]);
 
-
 const handleSchedule = () => {
   const token = sessionStorage.getItem('bearer');
 
-  // Create promises for PUT and DELETE requests
-  const requests = campDates.map(campdate => {
-      const isSelected = selectedDates.some(date => date.id === campdate.id);
+  const datesToPut = selectedDates.filter(date =>
+    !previousSelectedDates.some(prevDate => prevDate.id === date.id)
+  );
 
-      if (isSelected) {
-          // Send PUT request if the date is selected
-          return fetch(`${SERVER_URL}api/campgroup/${selectedGroup.id}/camptime/${campdate.id}`, {
-              method: 'PUT',
-              headers: { 
-                  Authorization: `Bearer ${token}`,
-                  'Content-Type': 'application/json'
-              },
-          });
-      } else {
-          // Send DELETE request if the date is not selected
-          return fetch(`${SERVER_URL}api/campgroup/${selectedGroup.id}/camptime/${campdate.id}`, {
-              method: 'DELETE',
-              headers: { 
-                  Authorization: `Bearer ${token}`,
-                  'Content-Type': 'application/json'
-              },
-          });
-      }
+  const datesToDelete = previousSelectedDates.filter(prevDate =>
+    !selectedDates.some(date => date.id === prevDate.id)
+  );
+
+  const putRequests = datesToPut.map(campdate => {
+    return fetch(`${SERVER_URL}api/campgroup/${selectedGroup.id}/camptime/${campdate.id}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+    });
   });
 
-  // Execute all requests and handle results
-  Promise.all(requests)
+  const deleteRequests = datesToDelete.map(campdate => {
+    return fetch(`${SERVER_URL}api/campgroup/${selectedGroup.id}/camptime/${campdate.id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+    });
+  });
+
+  Promise.all([...putRequests, ...deleteRequests])
       .then(responses => {
-          // Handle responses here, e.g., check for errors, reload data, etc.
           console.log('All operations completed');
-          fetchDates(); // Refresh dates after changes
+          setPreviousSelectedDates(selectedDates);
+          console.log("Pre selected dates");
+          console.log(previousSelectedDates);
+          fetchDates();
       })
       .catch(err => console.error('Error with scheduling operations:', err));
-};
-
-const handleGroupChange = (event) => {
-  const group = event.target.value;
-  setSelectedGroup(group);
-  handleGroupChangeAfterSelection(group);
 };
 
 const theme = createTheme({
